4fd1ef5eaca058b2cc97096d14183cd46062b25d
The B <bekirhan.kurt@outlook.com>
2025-09-19 01:00:08 +0300

    Brand feature added

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
+
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        public Task<CreatedBrandResponse>? Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse createdBrandResponse = new()
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name
+            };
+
+            return null;
+        }
+    }
+}
+
